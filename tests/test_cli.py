7971d298c21852dad94753d7ed13c90cf1b376b7
Tomas Karabela <tkarabela@seznam.cz>
2026-09-25 21:35:10 +0200

    chore: Add a test that calls CLI class directly
    This should help with test code coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,11 @@
+import os
 import subprocess
 import sys
 from pathlib import Path
 
 import pytest
 
+from handcron.cli import CLI
 from handcron.data import WorkerType
 
 DATA_DIR = Path(__file__).resolve().parent.joinpath("data")
@@ -11,7 +13,7 @@ DATA_DIR = Path(__file__).resolve().parent.joinpath("data")
 
 @pytest.mark.parametrize("cron", ["demo_memory.cron", "demo_sqlite.cron"])
 @pytest.mark.parametrize("worker", [WorkerType.SIMPLE, WorkerType.PROCESS])
-def test_demo_memory(worker: WorkerType, cron: str):
+def test_demo_by_running_module(worker: WorkerType, cron: str):
     cmd = [
         sys.executable,
         "-m",
@@ -22,3 +24,21 @@ def test_demo_memory(worker: WorkerType, cron: str):
         cron,
     ]
     subprocess.run(cmd, check=True, timeout=30, cwd=DATA_DIR)
+
+
+@pytest.mark.timeout(30)
+@pytest.mark.parametrize("cron", ["demo_memory.cron", "demo_sqlite.cron"])
+@pytest.mark.parametrize("worker", [WorkerType.SIMPLE, WorkerType.PROCESS])
+def test_demo_by_invoking_cli_from_python(worker: WorkerType, cron: str):
+    cmd = [
+        "tick",
+        "--worker",
+        worker.value,
+        cron,
+    ]
+    old_cwd = os.getcwd()
+    try:
+        os.chdir(DATA_DIR)
+        assert CLI().run(cmd) == 0
+    finally:
+        os.chdir(old_cwd)
